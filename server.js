aa7b066e023f79d6120c1ede941aa00a37b2e7fc
Nate Wolfe <nwolfe@puppet.com>
2018-06-06 14:19:06 -0700

    Update server responses

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const PORT = process.env.PORT || 8080;
 let userRouter = express.Router()
 
 userRouter.get('*', (req, res) => {
-	res.status(200).json({hello: 'world'})
+	res.status(200).json({hello: 'Nate, you have been onboarded'})
 });
 
 app.use(userRouter)
 
 app.listen(PORT, () => {
-	console.log('Hello PuppetConf')
+	console.log('Hello Nate, welcome to Pipelines For Containers')
 });
